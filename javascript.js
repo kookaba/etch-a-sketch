84ad4a71564c1e22bdfdbf5a7df4162f15c07094
Daniel <114618893+kookaba@users.noreply.github.com>
2023-02-08 11:22:14 -0600

    updated html, css, and js
    - separated JavaScript into javascipt.js
    - fixed TypeError from EventListeners by changing from for-in to for-of
    - added function and EventListener for paint()

---
+++ javascript.js
@@ -0,0 +1,61 @@
+const container = document.querySelector('.container');
+container.style.display = 'flex';
+container.style.flexDirection = 'row';
+container.style.border = '1px solid black';
+const gridBoxes = document.getElementsByClassName('grid-box');
+var currentColor = 'black';
+console.log(gridBoxes);
+
+function createGrid(size) {
+
+    for (x=1; x<size+1; x++) {
+        var newCol = document.createElement('div')
+        newCol.style.display = 'flex';
+        newCol.style.flexDirection = 'column';
+        newCol.style.flex = '1 1 auto';
+        newCol.id = 'column'+x;
+        container.appendChild(newCol);
+        for (y=1; y<size+1; y++) {
+            var newBox = document.createElement('div')
+            newBox.id = 'row'+x+'box'+y;
+            newBox.class = 'grid-box';
+            newBox.classList.add('grid-box');
+            newCol.appendChild(newBox);
+        }
+    }
+
+
+}
+
+//adds and removes the .hover class from grid boxes creating a shadow effect when cursor is hovering
+function toggleHover() {
+    this.classList.toggle('hover');
+}
+
+function paint() {
+    this.style.background = 'black';
+}
+
+//calls createGrid with size 16x16. 256 is the max
+createGrid(4);
+
+//adds event listeners to all grid boxes
+console.log('hello');
+for (const box of gridBoxes) {
+    console.log(box);
+    box.addEventListener('mouseenter', toggleHover, false);
+    box.addEventListener('mouseleave', toggleHover, false);
+    box.addEventListener('click', this.paint, false);
+}
+
+console.log('hello');
+
+/*
+var gridBoxes = document.getElementsByClassName('grid-box');
+for (i in gridBoxes) {
+    console.log(gridBoxes[i]);
+    gridBoxes[i].addEventListener('mouseenter', toggleHover, false);
+    gridBoxes[i].addEventListener('mouseleave', toggleHover, false);
+    gridBoxes[i].addEventListener('click', toggleHover, false);
+}
+*/
